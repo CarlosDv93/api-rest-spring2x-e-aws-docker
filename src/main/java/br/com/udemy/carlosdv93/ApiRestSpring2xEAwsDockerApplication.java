56246b0b36b630dd107e525c23441857dacab99a
Carlos D A Arcos <carlos9_cv@hotmail.com>
2021-09-05 14:06:20 -0300

    First Commit - Hello World

---
+++ src/main/java/br/com/udemy/carlosdv93/ApiRestSpring2xEAwsDockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.carlosdv93;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpring2xEAwsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpring2xEAwsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/udemy/carlosdv93/ApiRestSpring2xEAwsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.carlosdv93;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpring2xEAwsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
