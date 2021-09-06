09225ad90b50f2dc5b3eed35aa385bc8816cf32e
Carlos D A Arcos <carlos9_cv@hotmail.com>
2021-09-06 15:20:45 -0300

    Corrigindo Math Controller - até ula 60

--- src/main/java/br/com/udemy/carlosdv93/controllers/CalculatorController.java
+++ src/main/java/br/com/udemy/carlosdv93/controllers/CalculatorController.java
@@ -1,6 +1,5 @@
 package br.com.udemy.carlosdv93.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,21 +7,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
-
-    @RequestMapping("/calculator")
-    public String hello() {
-        System.out.println("Entrou no calculator");
-        return "Hello World calculator";
-    }
+    //Nome usado no curso MathController
 
     @RequestMapping("/sum/{number1}/{number2}")
-    public Double sum(@PathVariable(name = "number1") Double num1, @PathVariable(name = "number2") Double num2) {
+    public Double sum(@PathVariable(name = "number1") String num1, @PathVariable(name = "number2") String num2) throws Exception {
         System.out.println("Entrou no sum");
-        return num1+num2;
+        if(!isNumeric(num1) || !isNumeric(num2)) {
+            throw new Exception("Não é um numero!");
+        }
+
+        return convertToDouble(num1) + convertToDouble(num2);
     }
 
     @RequestMapping(value= "/sub/{number1}/{number2}", method = RequestMethod.GET)
-    public Double subs(@PathVariable(name = "number1") Double num1, @PathVariable(name = "number2") Double num2) {
-        return 20D;
+    public Double subs(@PathVariable(name = "number1") String num1, @PathVariable(name = "number2") String num2) throws Exception {
+        if(!isNumeric(num1) || !isNumeric(num2)){
+            throw new Exception("Não é um numero!");
+        }
+
+        return convertToDouble(num1) - convertToDouble(num2);
+    }
+
+    private Double convertToDouble(String strNum) {
+        if(strNum == null) return 0D;
+        String number = strNum.replaceAll(",", ".");
+
+        if(isNumeric(number)) return Double.parseDouble(number);
+
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[+-]?[0-9]+\\.?[0-9]+");
     }
 }
