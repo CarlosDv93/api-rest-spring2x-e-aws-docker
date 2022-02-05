54af7e0ca83d2100b3382b15963929ce4b680960
Carlos D A Arcos <carlos9_cv@hotmail.com>
2022-02-05 13:12:58 -0300

    Aula 71 - Implementando a primeira entity

---
+++ src/main/java/br/com/udemy/carlosdv93/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package br.com.udemy.carlosdv93.repository;
+
+import br.com.udemy.carlosdv93.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
